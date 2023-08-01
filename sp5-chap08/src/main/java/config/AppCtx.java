8c684d22c26c2e7cb5ff26ea8b7a6f06c25d649f
leehj <leehj@localhost>
2023-08-02 02:47:46 +0900

    2023-08-01 chap08 main.Main.class 책 안보고 작성하기

--- sp5-chap08/src/main/java/config/AppCtx.java
+++ sp5-chap08/src/main/java/config/AppCtx.java
@@ -47,26 +47,26 @@ public class AppCtx {
 
 	@Bean
 	public MemberRegisterService memberRegSvc() {
-		return new MemberRegisterService(memberDao());
+		return new MemberRegisterService(memberDao()) ;
 	}
-
+	
 	@Bean
 	public ChangePasswordService changePwdSvc() {
 		ChangePasswordService pwdSvc = new ChangePasswordService();
 		pwdSvc.setMemberDao(memberDao());
 		return pwdSvc;
 	}
-
+	
 	@Bean
 	public MemberPrinter memberPrinter() {
 		return new MemberPrinter();
 	}
-
+	
 	@Bean
 	public MemberListPrinter listPrinter() {
-		return new MemberListPrinter(memberDao(), memberPrinter());
+		return new MemberListPrinter(memberDao(),memberPrinter());
 	}
-
+	
 	@Bean
 	public MemberInfoPrinter infoPrinter() {
 		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
